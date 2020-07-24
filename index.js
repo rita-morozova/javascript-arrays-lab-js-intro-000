6405e58dc1bbf8f020a57a2fb7c12c3249d1a5eb
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-24 19:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  newArray = kittens.slice(0, kittens.length-1)
+  var newArray = kittens.slice(0, kittens.length-1)
   return newArray
 }
 
